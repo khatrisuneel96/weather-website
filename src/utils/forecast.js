bc3bc61a1a2da3b5d404f740d0acbcffa65a2c74
Suneel Kumar Khatri <sunil.genani@gmail.com>
2020-06-12 02:05:59 +0500

    add feelslike and humidity to forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback)=>{
         } else if (body.error){
             callback('Unable to find location. Try another search!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '! This is Currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '! This is Currently ' + body.current.temperature + ' degrees out, and it feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '% There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
